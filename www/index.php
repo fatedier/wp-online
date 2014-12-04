18bb6743a4d02e2d06252554ea187525d1379910
fatedier <512654112@qq.com>
2014-12-04 23:22:23 +0800

    Advance the method to check charset

--- www/index.php
+++ www/index.php
@@ -6,35 +6,30 @@
     $pageContents = "";
     
     /* 如果前7个字符不是 "http://"，则补上 */
-    if (substr($web_url, 0, 7) != "http://" && substr($web_url, 0, 5) != "https" && $web_url != "")
-    {
+    if (substr($web_url, 0, 7) != "http://" && substr($web_url, 0, 5) != "https" && $web_url != "") {
         $web_url = "http://".$web_url;
     }
     
-    if ($web_url != "")
-    {
+    if ($web_url != "") {
         $pageContents = HttpClient::quickGet($web_url);
         
-        /* 查找对应文本编码，替换当前页面的charset设置 */
-        $check_gb2312 = stripos($pageContents, "gb2312", 0);
-        $check_gbk = stripos($pageContents, "gbk", 0);
+        /* 需要做转换的网页编码格式 */
+        $charset_list = array("gb2312", "gbk");
 
-        if ($check_gb2312 != false)
-        {
-            $page_temp = substr($pageContents, $check_gb2312 - 20, $check_gb2312);
-            $check_charset = stripos($pageContents, "charset", 0);
-            if ($check_charset != false)
-            {
-                $charset = "gb2312";
-            }
-        }
-        else if ($check_gbk != false)
-        {
-            $page_temp = substr($pageContents, $check_gbk - 20, $check_gbk);
-            $check_charset = stripos($pageContents, "charset", 0);
-            if ($check_charset != false)
-            {
-                $charset = "gbk";
+        /* 查找是否存在指定编码 */
+        foreach ($charset_list as $charset_one) {
+            $charset_pos = stripos($pageContents, $charset_one, 0);
+            if ($charset_pos != false) {
+                /* 查找左括号的位置 */
+                $bracket_pos = strripos(substr($pageContents, 0, $charset_pos), "<", 0);
+                if ($bracket_pos != false) {
+                    /* 查找字符编码所在标签是否是meta */
+                    $meta_pos = stripos(substr($pageContents, $bracket_pos, $bracket_pos + 4), "meta");
+                    if ($meta_pos != false) {
+                        $charset = $charset_pos;
+                        break;
+                    }
+                }
             }
         }
     }
@@ -61,13 +56,14 @@
 </div>
 
 <?php
-    if ($web_url != "")
-    {
+    if ($web_url != "") {
 		echo("<div style=\"background-color:white;font-size:18px\" align=\"center\">");
         echo $web_url."<br/><br/>";
-		echo("</div>");
+		echo("</div>\n");
         
         /* 生成网址二维码 */
+		/* google api方式 */
+		/*
         function generateQRfromGoogle($chl, $widhtHeight = '80', $EC_level = 'L', $margin = '0') 
         { 
             echo '<img src="http://chart.apis.google.com/chart?chs='.$widhtHeight.'x'.$widhtHeight.'&cht=qr&chld='.$EC_level.'|'.$margin.'&chl='.$chl.'" alt="QR code" widhtHeight="'.$size.'" widhtHeight="'.$size.'"/>';
@@ -75,8 +71,14 @@
         echo("<div style=\"background-color:white;font-size:18px\" align=\"center\">");
         generateQRfromGoogle($web_url);
         echo("</div>");
+        */
         
-        echo "<div>".$pageContents."<br/></div>";
+        /* 二维码，img标签的src为生成二维码的php文件 */
+        echo("<div style=\"background-color:white;\" align=\"center\">");
+        echo("<img src=\"qrcode/get_qr_png.php?url=$web_url\" alt=\"qrpng\" /><br/><br/>");
+        echo("</div>");
+        
+        echo "<div>".$pageContents."</div>";
     }
 ?>
 
